91ea7032ac314dac1e2ab0a8a382e5f820896289
khacodee <114900252+khacodee@users.noreply.github.com>
2023-11-16 08:41:40 +0700

    Update Room API

--- HotelBookingApp/src/main/java/com/khacv/hotelbookingapp/controller/RoomController.java
+++ HotelBookingApp/src/main/java/com/khacv/hotelbookingapp/controller/RoomController.java
@@ -1,13 +1,11 @@
 package com.khacv.hotelbookingapp.controller;
 
+import com.khacv.hotelbookingapp.dto.RoomDTO;
 import com.khacv.hotelbookingapp.entity.Room;
 import com.khacv.hotelbookingapp.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,14 @@ public class RoomController {
         return ResponseEntity.ok(roomService.getRoomById(id));
     }
 
+    @PostMapping("/rooms")
+    public ResponseEntity<?> createRoom(@RequestBody RoomDTO roomDTO){
+        return ResponseEntity.ok(roomService.createRoom(roomDTO));
+    }
 
 
+    @PutMapping("/rooms/{id}")
+    public ResponseEntity<?> updateRoom(@PathVariable int id ,@RequestBody RoomDTO roomUpdate){
+        return ResponseEntity.ok(roomService.updateRoom(id, roomUpdate));
+    }
 }
